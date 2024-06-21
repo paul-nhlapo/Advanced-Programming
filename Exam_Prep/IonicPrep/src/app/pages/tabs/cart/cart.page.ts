84ccb80858a64f004df14106f5011613c517ac34
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2024-06-21 14:03:50 +0200

    Documenting

--- Exam_Prep/IonicPrep/src/app/pages/tabs/cart/cart.page.ts
+++ Exam_Prep/IonicPrep/src/app/pages/tabs/cart/cart.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { CartSubScription } from '../Model/cartSubscription';
 import { Subscription } from '../Model/subscriptionModel';
 import { SubscriptionCartOrganiserService } from '../../services/SubscriptionCartOrganiserService';
@@ -10,41 +9,45 @@ import { SubscriptionCartOrganiserService } from '../../services/SubscriptionCar
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
-  subscriptionsInCart : CartSubScription [] = [];
-  totalCostOfSubcriptionsInCart :number = 0;
-  isModalOpen = false;
-  constructor(private cartManager : SubscriptionCartOrganiserService) {
-    this.loadSubscriptions();
+  subscriptionsInCart: CartSubScription[] = []; // Array to store subscriptions in the cart
+  totalCostOfSubcriptionsInCart: number = 0; // Total cost of subscriptions in the cart
+  isModalOpen = false; // Flag to indicate if the modal is open or not
+
+  constructor(private cartManager: SubscriptionCartOrganiserService) {
+    this.loadSubscriptions(); // Load subscriptions when the component is initialized
+
+    // Subscribe to changes in the cart products number and reload subscriptions when it changes
     cartManager.cartProductsNumberDS.subscribe(num => {
-        this.loadSubscriptions();
+      this.loadSubscriptions();
     });
   }
-  
+
   ngOnInit(): void {
+    // Initialization logic goes here
   }
+
   setOpen(isOpen: boolean) {
-    this.isModalOpen = isOpen;
+    this.isModalOpen = isOpen; // Set the flag to indicate if the modal is open or not
   }
 
   loadSubscriptions() {
-    this.subscriptionsInCart = this.cartManager.getSubscriptionsInCart();
-    this.totalCostOfSubcriptionsInCart = this.cartManager.getTotalCostOfSubcriptionsInCart();
+    this.subscriptionsInCart = this.cartManager.getSubscriptionsInCart(); // Load subscriptions from the cart
+    this.totalCostOfSubcriptionsInCart = this.cartManager.getTotalCostOfSubcriptionsInCart(); // Calculate the total cost of subscriptions in the cart
   }
 
-  increaseProdCount (sub : Subscription) {
+  increaseProdCount(sub: Subscription) {
     for (var idx = 0; idx < this.subscriptionsInCart.length; idx++) {
       if (this.subscriptionsInCart[idx].subscription.id == sub.id) {
-        this.cartManager.addProdFromCart(this.subscriptionsInCart[idx].subscription);
+        this.cartManager.addProdFromCart(this.subscriptionsInCart[idx].subscription); // Increase the count of the selected subscription in the cart
       }
     }
   }
 
-  reduceProdCount (sub : Subscription) {
+  reduceProdCount(sub: Subscription) {
     for (var idx = 0; idx < this.subscriptionsInCart.length; idx++) {
       if (this.subscriptionsInCart[idx].subscription.id == sub.id) {
-         this.cartManager.removeProdFromCart(this.subscriptionsInCart[idx].subscription);
+        this.cartManager.removeProdFromCart(this.subscriptionsInCart[idx].subscription); // Decrease the count of the selected subscription in the cart
       }
     }
   }
-
 }
--- Exam_Prep/IonicPrep/src/app/pages/tabs/tabs-routing.module.ts
+++ Exam_Prep/IonicPrep/src/app/pages/tabs/tabs-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
@@ -8,17 +7,19 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children:[
-  {
-
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m=> m.HomePageModule)
+      // Child route for 'home' page
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+      },
+      // Child route for 'cart' page
+      {
+        path: 'cart',
+        loadChildren: () => import('./cart/cart.module').then(m => m.CartPageModule)
+      }
+    ]
   },
-  {
-    path: 'cart',
-    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
-  }
- ]
-  },
+  // Default route redirects to 'tabs/home'
   {
     path: '',
     redirectTo: 'tabs/home',
