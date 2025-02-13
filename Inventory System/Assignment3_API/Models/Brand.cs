8fd2c07b9070264fe071adad01d01c552a4bedbb
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2025-02-13 12:46:14 +0200

    Cleaning up

--- Inventory System/Assignment3_API/Models/Brand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Assignment3_Backend.Models
-{
-    public class Brand : BaseEntity
-    {
-        public int BrandId { get; set; }
-        public virtual ICollection<Product> Products { get; set; }
-    }
-}
--- Inventory System/Assignment3_API/ViewModels/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assignment3_Backend.ViewModels
-{
-    public class UserViewModel
-    {
-        public string? emailaddress { get; set; }
-        public string? password { get; set; }
-
-    }
-}
